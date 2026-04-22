cb4b8d596d05eb358f542dff2ed7ea8aef7473ff
David Awad <awaddavid65@gmail.com>
2026-04-22 18:27:12 +1000

    CSP: allow gstatic.com images for Google Pay button icon

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
                 + " https://applepay.cdn-apple.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://web.squarecdn.com https://sandbox.web.squarecdn.com",
               "font-src 'self' https://fonts.gstatic.com https://web.squarecdn.com https://sandbox.web.squarecdn.com https://square-fonts-production-f.squarecdn.com https://d1g145x70srn7h.cloudfront.net https://cash-f.squarecdn.com",
-              "img-src 'self' data: blob: https://web.squarecdn.com",
+              "img-src 'self' data: blob: https://web.squarecdn.com https://www.gstatic.com https://pay.google.com",
             ].join("; "),
           },
           { key: "X-Frame-Options",         value: "DENY" },
