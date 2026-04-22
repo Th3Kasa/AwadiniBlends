23f87a03f221c556fcca9b4f962ad216faf222a9
David Awad <awaddavid65@gmail.com>
2026-04-22 18:25:15 +1000

    CSP: allow google.com in connect-src for Google Pay

--- next.config.js
+++ next.config.js
@@ -25,6 +25,8 @@ const nextConfig = {
                 + " https://nominatim.openstreetmap.org"
                 + " https://o160250.ingest.sentry.io"
                 + " https://pay.google.com"
+                + " https://google.com"
+                + " https://www.google.com"
                 + " https://applepay.cdn-apple.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://web.squarecdn.com https://sandbox.web.squarecdn.com",
               "font-src 'self' https://fonts.gstatic.com https://web.squarecdn.com https://sandbox.web.squarecdn.com https://square-fonts-production-f.squarecdn.com https://d1g145x70srn7h.cloudfront.net https://cash-f.squarecdn.com",
